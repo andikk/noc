e603e0cfdece909b3702e57b4ab051113125ab63
andikk <andikk@mail.ru>
2020-08-04 16:17:48 +0300

    first commit

---
+++ source/js/bundle/custom.js
@@ -0,0 +1,14 @@
+ $(function(){
+
+   $('.js-menu-btn').click(function() {
+     $('.navigation__list').toggleClass('opened'); // Открываем верхнее меню
+     $('.js-menu-btn').toggleClass('js-change'); // Меняем икноку на кнопке
+   });
+
+   $('.navigation__link').click(function() {
+     $('.navigation__list').toggleClass('opened'); // Открываем верхнее меню
+     $('.js-menu-btn').toggleClass('js-change'); // Меняем икноку на кнопке
+   });
+
+
+});
